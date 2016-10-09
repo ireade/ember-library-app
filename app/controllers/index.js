1b7e0967560e739957259000d8b0e8bdd8dad6f0
Ire Aderinokun <ire@ireaderinokun.com>
2016-10-09 19:38:48 +0100

    initial commit

---
+++ app/controllers/index.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+  emailAddress: '',
+
+  isValid: Ember.computed.match('emailAddress', /^.+@.+\..+$/),
+  isDisabled: Ember.computed.not('isValid'),
+
+  actualEmailAddress: Ember.computed('emailAddress', function() {
+    console.log('actualEmailAddress function is called: ', this.get('emailAddress'));
+  }),
+
+  emailAddressChanged: Ember.observer('emailAddress', function() {
+    console.log('observer is called', this.get('emailAddress'));
+  }),
+
+  actions: {
+
+    saveInvitation() {
+      const email = this.get('emailAddress');
+
+      const newInvitation = this.store.createRecord('invitation', {
+        email: email
+      });
+
+      newInvitation.save().then((response) => {
+        this.set('responseMessage', `Thank you! We saved your email address with the following id: ${response.get('id')}`);
+        this.set('emailAddress', '');
+      });
+    }
+  }
+
+});
---
+++ config/environment.js
@@ -0,0 +1,61 @@
+/* jshint node: true */
+
+module.exports = function(environment) {
+  var ENV = {
+    modulePrefix: 'library-app',
+    environment: environment,
+    rootURL: '/',
+    locationType: 'auto',
+
+    firebase: {
+      apiKey: "AIzaSyDen_LLjUct7DYtGqP3C5wXMn7zkN9OrN4",
+      authDomain: "ember-library-app-e75fe.firebaseapp.com",
+      databaseURL: "https://ember-library-app-e75fe.firebaseio.com",
+      storageBucket: "ember-library-app-e75fe.appspot.com"
+    },
+
+    // if using ember-cli-content-security-policy
+    contentSecurityPolicy: {
+      'script-src': "'self' 'unsafe-eval' apis.google.com",
+      'frame-src': "'self' https://*.firebaseapp.com",
+      'connect-src': "'self' wss://*.firebaseio.com https://*.googleapis.com"
+    },
+
+    EmberENV: {
+      FEATURES: {
+        // Here you can enable experimental features on an ember canary build
+        // e.g. 'with-controller': true
+      }
+    },
+
+    APP: {
+      // Here you can pass flags/options to your application instance
+      // when it is created
+    }
+  };
+
+  if (environment === 'development') {
+    // ENV.APP.LOG_RESOLVER = true;
+    ENV.APP.LOG_ACTIVE_GENERATION = true;
+    ENV.APP.LOG_TRANSITIONS = true;
+    ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    ENV.APP.LOG_VIEW_LOOKUPS = true;
+  }
+
+  if (environment === 'test') {
+    // Testem prefers this...
+    ENV.locationType = 'none';
+
+    // keep test console output quieter
+    ENV.APP.LOG_ACTIVE_GENERATION = false;
+    ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    ENV.APP.rootElement = '#ember-testing';
+  }
+
+  if (environment === 'production') {
+
+  }
+
+  return ENV;
+};
